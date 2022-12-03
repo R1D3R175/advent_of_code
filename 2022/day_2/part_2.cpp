6711aa7601d2644b50189a9d8d81e41a2c34f644
R1D3R175 <r1.d3.r175@gmail.com>
2022-12-03 16:39:01 +0100

    Added solution for day 2 part 2

---
+++ 2022/day_2/part_2.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+
+static const short ROCK = 0b001, PAPER = 0b010, SCISSORS = 0b100;
+static const short LOSE = 0b001, DRAW = 0b010, WIN = 0b100;
+
+int main(int argc, char** argv) {
+    std::ifstream in("input.txt");
+    std::ofstream out("output.txt");
+
+    size_t total_score = 0;
+
+    char opponent_move, my_move;
+    unsigned short opponent, me;
+    while(in >> opponent_move) {
+        in >> my_move;
+
+        opponent = 1 << ((int)(opponent_move) - (int)('A'));
+        me = 1 << ((int)(my_move) - (int)('X'));
+
+        switch(me) {
+            case LOSE:
+                me = 1 << ((opponent == ROCK) ? (PAPER) : ((opponent == SCISSORS) ? (ROCK) : (0)));
+                break;
+            case DRAW:
+                me = opponent;
+                total_score += 3;
+                break;
+            case WIN:
+                me = 1 << ((opponent != SCISSORS) ? (opponent) : (0));
+                total_score += 6;
+                break;
+        }
+
+        total_score += (me != 4) ? (me) : (me - 1);
+    }
+
+    out << total_score << std::endl;
+
+    in.close();
+    out.close();
+}
