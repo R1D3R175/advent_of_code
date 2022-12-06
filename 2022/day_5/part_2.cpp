7f71d1a5f5d8074e481f5f3a3adc77d70bd31cec
Ben <54753989+R1D3R175@users.noreply.github.com>
2022-12-06 11:39:56 +0100

    Added solution for day 5 part 2

---
+++ 2022/day_5/part_2.cpp
@@ -0,0 +1,77 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+static inline bool is_data(const std::string& str) {
+    return (str.find_first_not_of(str[0]) != std::string::npos);
+}
+
+static inline size_t substr_to_ul(const std::string& str,
+                                  const size_t& start_whitespace,
+                                  const size_t& end_whitespace)
+{
+    return std::stoul(str.substr(start_whitespace + 1, end_whitespace - start_whitespace - 1));
+}
+
+int main(int argc, char** argv) {
+    std::ifstream in("input.txt");
+    std::ofstream out("output.txt");
+
+    std::vector<std::deque<char>> crates;
+
+    std::string line, tmp;
+    size_t pos;
+
+    while(std::getline(in, line)) {
+        if(line.find('1') != std::string::npos) {
+            std::getline(in, line);
+            break;
+        }
+
+        size_t tmp_size = (size_t)(ceil(line.length() / 4.0));
+        if(tmp_size > crates.size())
+            crates.resize(tmp_size);
+
+        for(pos = 0; pos < tmp_size; pos++) {
+            tmp = line.substr(pos * 4, 4);
+            if(!is_data(tmp)) continue;
+
+            crates[pos].push_back(tmp[1]);
+        }
+    }
+
+    while(std::getline(in, line)) {
+        size_t how_much_start = 4;
+        size_t how_much_end = line.find(' ', how_much_start + 1);
+        size_t how_much = substr_to_ul(line, how_much_start, how_much_end);
+
+        size_t source_start = how_much_end + 5;
+        size_t source_end = line.find(' ', source_start + 1);
+        size_t source = substr_to_ul(line, source_start, source_end) - 1;
+
+        size_t dest_start = source_end + 3;
+        size_t dest_end = std::string::npos;
+        size_t dest = substr_to_ul(line, dest_start, dest_end) - 1;
+
+        for(size_t i = 0; i < how_much; i++) {
+            if(crates[source].empty()) break;
+
+            auto source_elem_it = crates[source].begin() + (how_much - i - 1);
+
+            crates[dest].push_front(*source_elem_it);
+            crates[source].erase(source_elem_it);
+        }
+    }
+
+    for(const auto& crate : crates) {
+        if(crate.empty()) continue;
+
+        out << crate.front();
+    }
+
+    in.close();
+    out.close();
+    return 0;
+}
