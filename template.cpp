7a467ab71810fdafb0b69a89c31c22b7ac75b7ce
R1D3R175 <r1.d3.r175@gmail.com>
2024-02-09 04:32:37 +0100

    feat: update template to use a class with logging support

--- template.cpp
+++ template.cpp
@@ -1,12 +1,64 @@
+#include <iostream>
 #include <fstream>
+#include <source_location>
+
+class Solver
+{
+    private:
+        std::ifstream& in;
+        std::ofstream& out;
+        bool enable_debug;
+
+        void log(const std::string& message, const std::source_location& location = std::source_location::current());
+        void solve();
+
+    public:
+        Solver(std::ifstream& in, std::ofstream& out) : in(in), out(out), enable_debug(false) { this->solve(); }
+        Solver(std::ifstream& in, std::ofstream& out, bool enable_debug) : in(in), out(out), enable_debug(enable_debug) {}
+        ~Solver();
+};
+
+Solver::~Solver()
+{
+    in.close();
+    out.close();
+}
+
+void Solver::log(const std::string& message, const std::source_location& location)
+{
+    if(this->enable_debug)
+    {
+        std::cout << "DEBUG [" << location.function_name() << "] -> " << message << std::endl;
+    }
+}
+
+void Solver::solve()
+{
+    // Your code here...
+}
 
 int main(int argc, char** argv)
 {
-    std::ifstream in("input.txt");
-    std::ofstream out("output.txt");
+    if(argc < 3 || argc > 4)
+    {
+        std::cout << "Usage: " << argv[0] << " <input file> <output file> [debug, write anything to enable it]" << std::endl;
+        return -1;
+    }
+    bool debug = argc == 4;
 
-    // Your code here...
+    std::ifstream in(argv[1]);
+    if(!in.is_open())
+    {
+        std::cout << "File " << argv[1] << " does not exist." << std::endl;
+        return -1;
+    }
 
-    in.close();
-    out.close();
+    std::ofstream out(argv[2]);
+    if(!out.is_open())
+    {
+        std::cout << "File " << argv[2] << " could not be created." << std::endl;
+        return -1;
+    }
+
+    Solver(in, out, debug);
 }
